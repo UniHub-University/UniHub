9d14dc9c6ec4a6c5377aae4078926ee583bff3c3
PedroHenriqueNovello <phnporto@gmail.com>
2026-09-20 19:09:50 -0300

    feat: atualizando a entidade usuario após implementar ResticaoAlimentar

--- Backend/src/UniHub.Domain/Entities/Usuario.cs
+++ Backend/src/UniHub.Domain/Entities/Usuario.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace UniHub.Domain.Entities;
 
@@ -50,6 +51,8 @@ public class Usuario
 
     // solicitacoes de apoio feitas por este usuario (modulo Acao Solidaria)
     public ICollection<SolicitacaoApoio> Solicitacoes { get; set; } = new List<SolicitacaoApoio>();
+
+    public virtual ICollection<RestricaoAlimentar> RestricoesAlimentares { get; set; } = new List<RestricaoAlimentar>();
 
     /// Atualiza os dados basicos de cadastro do usuario (nome, telefone e foto).
     /// O email institucional e o GoogleId nao sao alterados aqui de proposito,
