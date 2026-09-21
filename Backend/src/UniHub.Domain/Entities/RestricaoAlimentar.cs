6d0e34535cda19ef6532e651c5d1b25e46a7224f
PedroHenriqueNovello <phnporto@gmail.com>
2026-09-21 16:14:27 -0300

    feat: fazendo as alterações após revisão do código

--- Backend/src/UniHub.Domain/Entities/RestricaoAlimentar.cs
+++ Backend/src/UniHub.Domain/Entities/RestricaoAlimentar.cs
@@ -6,12 +6,12 @@ namespace UniHub.Domain.Entities
 {
     public class RestricaoAlimentar
     {
-        public Guid Id {get; set;}
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Nome {get; set;} = string. Empty;
         public string? Descricao {get; set;}
 
         // Propriedade de navegação para o relacionamento N:N com Usuario
-        public virtual ICollection<Usuario> Ususarios {get; set;} = new List<Usuario>();
-        
+        public virtual ICollection<Usuario> Usuarios {get; set;} = new List<Usuario>();
+
     }
 }
