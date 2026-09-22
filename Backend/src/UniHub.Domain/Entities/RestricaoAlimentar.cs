66405ad33205ba9c372ef366e3219efbf912dfe6
Henrique Rodrigues Ribeiro <rique.ro.ri@gmail.com>
2026-09-21 23:19:16 -0300

    Fix spacing in RestricaoAlimentar class

--- Backend/src/UniHub.Domain/Entities/RestricaoAlimentar.cs
+++ Backend/src/UniHub.Domain/Entities/RestricaoAlimentar.cs
@@ -7,7 +7,7 @@ namespace UniHub.Domain.Entities
     public class RestricaoAlimentar
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public string Nome {get; set;} = string. Empty;
+        public string Nome {get; set;} = string.Empty;
         public string? Descricao {get; set;}
 
         // Propriedade de navegação para o relacionamento N:N com Usuario
